0a6081e1b28aa82097ac6f7d5c052eda08cd6b49
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-13 23:14:14 +0200

    [UPD] cards done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, } from 'react'
 import axios from "axios"
 
 
@@ -14,7 +14,36 @@ function App() {
 
   return (
     <>
-      <div className="btn btn-primary" onClick={fetchActresses}>CARICA LA PAGINA</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center"><h1>react-api</h1></div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="btn btn-primary" onClick={fetchActresses}>LOAD THE PAGE</div>
+          </div>
+        </div>
+        <div className="row g-3">
+          {actresses.map((actress) => {
+            return <div key={`actress ${actress.id}`} className="col-12 col-md-6 col-lg-4">
+              <div className="card d-flex flex-row">
+                <div className="card-image">
+                  <img className='' src={actress.image} alt="" />
+                </div>
+                <div className="card-infos">
+                  <p>{actress.name}</p>
+                  <p>{actress.birth_year}</p>
+                  <p>{actress.death_year}</p>
+                  <p>{actress.nationality}</p>
+                  <p>{actress.most_famous_movies}</p>
+                  <p>{actress.awards}</p>
+                  <p>{actress.biography}</p>
+                </div>
+              </div>
+            </div>
+          })}
+        </div>
+      </div>
     </>
   )
 }
